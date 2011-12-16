d1ae96e1231030b54f156e17068895f411c20834
Yu LIU <yliu@keley-consulting.com>
2011-12-16 16:10:36 +0100

    Added layout links

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,9 +14,9 @@ describe PagesController do
     end
 	
 	it "should have the right title" do
-		get 'home'
-		response.should have_selector("title", 
-									  :content => "#{@base_title} | Home")
+	  get 'home'
+	  response.should have_selector("title", 
+									 :content => "#{@base_title} | Home")
 	end
 	
 	it "should have a non-blank body" do
@@ -48,6 +48,19 @@ describe PagesController do
 		get 'about'
 		response.should have_selector("title", 
 									  :content => "#{@base_title} | About")
+	end
+  end
+	
+  describe "GET 'help'" do
+	it "returns http success" do
+	  get 'help'
+	  response.should be_success
+	end
+		
+	it "should have the right title" do
+		get 'help'
+		response.should have_selector("title", 
+									  :content => "#{@base_title} | Help")
 	end
   end
 
