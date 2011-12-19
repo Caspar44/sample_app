047af8dcbff2bd229921a1eb6213ced706d8259a
Yu LIU <yliu@keley-consulting.com>
2011-12-19 16:59:15 +0100

    Finished signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @title = @user.name
-end
-
+  end
+  
+  def create
+    #raise params[:user].inspect
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user, :flahs => {:success => "Welcome to the Sample App!"}
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
